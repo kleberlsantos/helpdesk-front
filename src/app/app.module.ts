b044f2fc1efdba783dc4a8824d73f774a12c9c2d
Kleber Santos <kleber.kerygma@gmail.com>
2022-11-28 21:43:31 -0300

    desenvolvomento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,10 +34,13 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
+//
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [ 
-    AppComponent, NavComponent, HomeComponent, HeaderComponent, TecnicoListComponent
+    AppComponent, NavComponent, HomeComponent, HeaderComponent, TecnicoListComponent, LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -64,14 +67,14 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
     MatTableModule,
     MatIconModule,
     MatListModule,
-    MatCardModule /*,    
+    MatCardModule ,    
     ToastrModule.forRoot({
       timeOut: 4000,
       closeButton: true,
       progressBar: true
-    }) ,
+    }) /*,
     NgxMaskModule.forRoot(),
-    */
+   */
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from 'src/app/models/credenciais';
+
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  creds: Credenciais={
+    email:"",
+    senha:""
+  }
+
+  email = new FormControl(null,Validators.email);
+  senha = new FormControl(null,Validators.minLength(3));
+
+  constructor(private toast:ToastrService){}
+
+  ngOnInit(): void {
+  }
+
+  logar(){
+    this.toast.error("Usuário e/ou senha inválidos!","Login");
+    this.creds.senha="";
+  }
+  
+
+  validaCampos():boolean{
+    return (this.email.valid && this.senha.valid);
+  }
+
+}
