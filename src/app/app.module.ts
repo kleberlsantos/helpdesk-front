e5fc5ebd76ec64ffd88a61044b9b6c808978ca74
Kleber Santos <kleber.kerygma@gmail.com>
2022-12-27 17:44:07 -0300

    desenvolvimento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,6 +50,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { NgxMaskModule } from 'ngx-mask';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 @NgModule({
   declarations: [
     AppComponent, NavComponent, HomeComponent, HeaderComponent,
@@ -58,7 +59,7 @@ import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-
 
     TecnicoListComponent, TecnicoCreateComponent, TecnicoUpdateComponent, TecnicoDeleteComponent,
 
-    ClienteListComponent, ClienteCreateComponent, ClienteUpdateComponent,ClienteDeleteComponent, ChamadoListComponent
+    ClienteListComponent, ClienteCreateComponent, ClienteUpdateComponent,ClienteDeleteComponent, ChamadoListComponent, ChamadoCreateComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Chamado } from 'src/app/models/chamado';
-import { Cliente } from 'src/app/models/cliente';
+import { ChamadoService } from 'src/app/services/chamado.service';
 
 @Component({
   selector: 'app-chamado-list',
@@ -12,7 +12,7 @@ import { Cliente } from 'src/app/models/cliente';
 export class ChamadoListComponent implements OnInit {
 
 
-  ELEMENT_DATA: Chamado[] = [{
+  ELEMENT_DATA: Chamado[] = [  /*{
     id: 1,
     titulo: "Chamado 1",
     cliente: "1",
@@ -25,30 +25,62 @@ export class ChamadoListComponent implements OnInit {
     tecnico: "6",
     nomeCliente: "Kleber Santos",
     nomeTecnico: "Albert Einstein"
-  }];
+  }*/];
+  FILTERED_DATA: Chamado[] = [];
 
   displayedColumns: string[] = ['id', 'titulo', 'cliente','tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  constructor(private service:ChamadoService) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
-  findAll() {
-   /* this.service.findAll().subscribe(resposta => {
+  findAll():void {
+    this.service.findAll().subscribe(resposta => {
       this.ELEMENT_DATA = resposta;
-      this.dataSource = new MatTableDataSource<Cliente>(this.ELEMENT_DATA);
+      this.dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
       this.dataSource.paginator = this.paginator;
     })
-    */
+    
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  retornaStatus(status:any):string{
+    if(status == "0"){
+      return "ABERTO";
+    } else if(status =="1"){
+      return "EM ANDAMENTO";
+    } else 
+      return "ENCERRADO";
+  }
+
+  retornaPrioridade(prioridade:any):string{
+    if(prioridade == "0"){
+      return "BAIXA";
+    } else if(prioridade =="1"){
+      return "MEDIA";
+    } else 
+      return "ALTA";
+  }
+
+  orderByStatus(status:any):void{
+    let list:Chamado[]=[];
+    this.ELEMENT_DATA.forEach(element => {
+      if(element.status==status)
+        list.push(element);
+    });
+    this.FILTERED_DATA = list;
+    
+    this.dataSource = new MatTableDataSource<Chamado>(this.FILTERED_DATA);
+    this.dataSource.paginator = this.paginator;
   }
 
 }
